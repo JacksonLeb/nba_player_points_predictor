0fc0c5fe3f919e693e57d6a14aeb6e078338920c
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-03-06 14:43:16 -0500

    fitting panda dfs into tensors for traininga nd testing

--- position_predictor.py
+++ position_predictor.py
@@ -1,8 +1,11 @@
+from base64 import encode
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import pandas as pd
 import math
+import random
 
 # gradient computation etc. not efficient for whole data set
 # -> divide dataset into small batches
@@ -31,12 +34,31 @@ class NBADataset(Dataset):
     def __init__(self):
         # Initialize data, download, etc.
         # read with numpy or pandas
-        xy = np.loadtxt('all_seasons.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        xy = pd.read_csv('2021-2022 NBA Player Stats.csv', delimiter=';', dtype=None, encoding = "ISO-8859-1")
+        print(xy.shape)
+        print(type(xy))
         self.n_samples = xy.shape[0]
+        x = xy.iloc[:, 5:].values
+        print(x)
 
-        # here the first column is the class label, the rest are the features
-        self.x_data = torch.from_numpy(xy[:, 1:]) # size [n_samples, n_features]
-        self.y_data = torch.from_numpy(xy[:, [0]]) # size [n_samples, 1]
+        y = xy.iloc[:, [2]].values
+        y = pd.DataFrame(y)
+        print(y.shape)
+        #finding position and frequencey for dummy values
+        print(y[0].value_counts())
+        #applying cat codes
+        y[0] = y[0].astype('category')
+        print(y)
+        print("DATA TYPE")
+        print(y[0].dtypes)
+        cat_column = y.select_dtypes(['category']).columns  
+        y[cat_column] = y[cat_column].apply(lambda x: x.cat.codes)
+        print(y)
+        print(y.shape)
+
+        # here the third column is the class label, the rest are the features
+        self.x_data = torch.tensor(x) # size [n_samples, n_features]
+        self.y_data = torch.tensor(y, dtype=torch.int) # size [n_samples, 1]
 
     # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
@@ -48,7 +70,7 @@ class NBADataset(Dataset):
 
 
 # create dataset
-dataset = WineDataset()
+dataset = NBADataset()
 
 # get first sample and unpack
 first_data = dataset[0]
@@ -78,22 +100,11 @@ print(total_samples, n_iterations)
 for epoch in range(num_epochs):
     for i, (inputs, labels) in enumerate(train_loader):
         
-        # here: 178 samples, batch_size = 4, n_iters=178/4=44.5 -> 45 iterations
         # Run your training process
         if (i+1) % 5 == 0:
             print(f'Epoch: {epoch+1}/{num_epochs}, Step {i+1}/{n_iterations}| Inputs {inputs.shape} | Labels {labels.shape}')
 
 # some famous datasets are available in torchvision.datasets
-# e.g. MNIST, Fashion-MNIST, CIFAR10, COCO
-
-train_dataset = torchvision.datasets.MNIST(root='./data', 
-                                           train=True, 
-                                           transform=torchvision.transforms.ToTensor(),  
-                                           download=True)
-
-train_loader = DataLoader(dataset=train_dataset, 
-                                           batch_size=3, 
-                                           shuffle=True)
 
 # look at one random sample
 dataiter = iter(train_loader)
